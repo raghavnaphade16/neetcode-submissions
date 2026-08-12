4e57adc857e8dc313b10d162cf89072067974d3e
raghavnaphade16 <66428787+raghavnaphade16@users.noreply.github.com>
2026-08-12 20:45:27 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for(int no : nums){
+        if(!set.add(no)){
+            return true;
+        }
+    }   
+    return false;
+    // }
+        // return false;
+        // Set<Integer> set = new HashSet<>();
+        // for(int num:nums){
+        //     if(!set.add(num)){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        // HashMap <Integer,Integer> hashMap = new HashMap<>();
+        // boolean isDuplicate = false;
+        // for(int no : nums){
+        //     if(hashMap.containsKey(no)){
+        //         isDuplicate = true;
+        //     } else {
+        //         hashMap.put(no,no);
+        //     }
+        // }
+        // return isDuplicate;
+    }
+}
